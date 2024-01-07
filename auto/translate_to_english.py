7c9ca9512a187b2d2115966734d801439191a450
weisiwu <2436887475@qq.com>
2024-01-07 17:05:14 +0800

    feat: format translate_to_english.py, following py file will comply with this format

--- auto/translate_to_english.py
+++ auto/translate_to_english.py
@@ -1,9 +1,14 @@
-import os
+def translate_to_english():
+    pass
 
-# import sys
 
-# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-from config_loader import loader_config
+if __name__ == "__main__":
+    import os
+    import sys
 
-config = loader_config()
-print(config)
+    sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+    from config_loader import loader_config
+
+    config = loader_config()
+    print(config)
+    translate_to_english()
