5c9e48db5fbd15aa0017e207f6e356fba99e7bf7
weisiwu <2436887475@qq.com>
2024-01-28 20:55:52 +0800

    feat: pyttsx3 生成中文语音

--- Dockerfile
+++ Dockerfile
@@ -13,14 +13,10 @@ RUN apt-get update && \
     apt-get install -y git vim wget ffmpeg espeak libespeak1 unzip
 
 # 下载espeak中文语言资料包
-RUN mkdir -p /novel_test/tmp/
-RUN git clone 'https://github.com/caixxiong/espeak-data' /novel_test/tmp/
-RUN cd /novel_test/tmp/ && unzip espeak-data.zip && mv espeak-data/* ..
-RUN cp -r /novel_test/tmp/* /usr/lib/x86_64-linux-gnu/espeak-data
-RUN rm -rf /novel_test/tmp
-
-# 不知道是否可以这样执行？
-# RUN ./init.py
+RUN cd /usr/lib/x86_64-linux-gnu/espeak-data
+RUN git clone 'https://github.com/caixxiong/espeak-data'
+RUN cd espeak-data/ && unzip espeak-data.zip && cp -r * /usr/lib/x86_64-linux-gnu/espeak-data
+RUN espeak --compile=zh
 
 # 设置工作目录为 /app
 WORKDIR /novel_test
--- scripts/generate_mp3_from_text.py
+++ scripts/generate_mp3_from_text.py
@@ -1,6 +1,6 @@
 import os
 import pyttsx3
-from pydub import AudioSegment
+import socketio
 from config_loader import get_mp3_config
 
 
@@ -10,30 +10,21 @@ def generate_by_gTTs():
 
 def generate_by_pyttsx3(text, speed, volumn, save_path):
     engine = pyttsx3.init()
-    # 设置引擎为中文
-    # engine.setProperty("voice", "zh")
+    engine.setProperty("voice", "zh")  # 设置引擎为中文
     engine.setProperty("rate", speed)  # 设置语速
     engine.setProperty("volume", volumn)  # 设置音量
 
-    # 标准的粤语发音
-    engine.setProperty("voice", "com.apple.speech.synthesis.voice.sin-ji")
-    # 普通话发音
-    # engine.setProperty("voice", "com.apple.speech.synthesis.voice.ting-ting.premium")
-    # 台湾甜美女生普通话发音
-    # engine.setProperty("voice", "com.apple.speech.synthesis.voice.mei-jia")
-
     # 由于是在docker容器中，并不存在声卡，所以需要创建虚拟声卡
     try:
-        print('保存路径', save_path)
+        print("保存路径", save_path)
         engine.save_to_file(text, save_path)
         engine.runAndWait()
+        # 长文本转换，需要分段执行
         # while "output.mp3" not in os.listdir(save_path):
         #     socketio.sleep(1)
     except Exception as e:
         print("发生错误", e)
 
-    # sound = AudioSegment.from_wav(wav_path)
-    # sound.export(mp3_path, format="mp3")
     pass
 
 
