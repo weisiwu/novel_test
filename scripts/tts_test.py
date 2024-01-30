8859aac81b4fed676433692063a1aa090212acb1
weisiwu <2436887475@qq.com>
2024-01-31 00:09:29 +0800

    feat: TTS docker 跑通

--- scripts/tts_test.py
+++ scripts/tts_test.py
@@ -3,6 +3,7 @@ from TTS.api import TTS
 
 # Get device
 device = "cuda" if torch.cuda.is_available() else "cpu"
+print("device", device)
 
 # List available 🐸TTS models
 print(TTS().list_models())
@@ -13,11 +14,24 @@ tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device)
 # Run TTS
 # ❗ Since this model is multi-lingual voice cloning model, we must set the target speaker_wav and language
 # Text to speech list of amplitude values as output
-wav = tts.tts(text="Hello world!", speaker_wav="my/cloning/audio.wav", language="en")
 # Text to speech to a file
 tts.tts_to_file(
-    text="Hello world!",
-    speaker_wav="my/cloning/audio.wav",
-    language="en",
+    # text="你好，我的阿斯顿发发啊!",
+    text="""
+君不见黄河之水天上来，奔流到海不复回。
+君不见高堂明镜悲白发，朝如青丝暮成雪。
+人生得意须尽欢，莫使金樽空对月。
+天生我材必有用，千金散尽还复来。
+烹羊宰牛且为乐，会须一饮三百杯。
+岑夫子，丹丘生，将进酒，杯莫停。
+与君歌一曲，请君为我倾耳听。
+钟鼓馔玉不足贵，但愿长醉不愿醒。
+古来圣贤皆寂寞，惟有饮者留其名。
+陈王昔时宴平乐，斗酒十千恣欢谑。
+主人何为言少钱，径须沽取对君酌。
+五花马、千金裘，呼儿将出换美酒，与尔同销万古愁。
+    """,
+    speaker_wav="sample.wav",
+    language="zh",
     file_path="output.wav",
 )
