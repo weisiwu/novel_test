ba19dfe683611e78b6c2ddace542b05e682fdac8
weisiwu <siwu.wsw@autonavi.com>
2024-02-02 17:50:25 +0800

    chore: 生成srt 初步调试

--- scripts/generate_srt_from_text.py
+++ scripts/generate_srt_from_text.py
@@ -1,60 +1,50 @@
-# 输入文字，生成srt
 import os
-import pysrt
-from pysrt import SubRipTime, SubRipItem
-from pydub import AudioSegment
-from scripts.config_loader import loader_config
+from pysrt import srttime, SubRipFile, SubRipTime, SubRipItem
+from novel_split import novel_split
+from config_loader import loader_config
 
 
-def process_text(paragraphs, subs):
-    async_tasks = []
+def generate_srt_from_text(segment_times, output_path):
+    subs = SubRipFile()
+    paragraphs = novel_split()
+    print("paragraphs===>", len(paragraphs), paragraphs)
+    print("segment_times===>", segment_times)
 
-    # 获取时间
-    for index, paragraph in enumerate(paragraphs):
-        audio = AudioSegment.from_file(filename)
-        audio_duration = len(audio) / 1000  # 转换为秒
+    # # 获取时间
+    for index, sgement_time in enumerate(segment_times):
+        sgement_time = sgement_time / 1000  # 转换为秒
+        print(f"{index}===> {sgement_time}秒")
 
         # 设置字幕开始和结束时间
-        start_time = pysrt.srttime.SubRipTime(milliseconds=0)
-        end_time = SubRipTime(seconds=audio_duration)
+        start_time = srttime.SubRipTime(milliseconds=0)
+        end_time = SubRipTime(seconds=sgement_time)
 
         # 如果已有字幕，取上一个字幕的结束时间作为当前字幕的开始时间
         if subs:
             start_time = subs[-1].end
             end_time = SubRipTime(
-                seconds=((subs[-1].end.ordinal / 1000) + audio_duration)
+                seconds=((subs[-1].end.ordinal / 1000) + sgement_time)
             )
 
         print(f"index: {index} start_time: {start_time} end_time: {end_time}")
         # 添加字幕
         subs.append(
             SubRipItem(
-                index=len(subs) + 1, start=start_time, end=end_time, text=paragraph
+                index=len(subs) + 1,
+                start=start_time,
+                end=end_time,
+                text=paragraphs[index],
             )
         )
+
+    # 保存字幕文件为 SRT 格式
+    subs.save(output_path)
 
 
 if __name__ == "__main__":
     config = loader_config()
-    input_path = config.get("input", {}).get("input_path", "")
-    novel_text = config.get("input", {}).get("input_text", "")
     output_path = config.get("output", {}).get("srt_path", "")
 
-    # 初始化 SRT 字幕对象
-    subs = pysrt.SubRipFile()
-    combined_audio = AudioSegment.silent()
-
-    # 运行异步程序
-    process_text(novel_text, subs, output_path)
-
-    for index in range(len(subs)):
-        filename = os.path.join(
-            os.path.abspath(os.path.dirname(__file__)),
-            config["output"]["save_path"],
-            f"{config['output']['name']}_{index}.mp3",
-        )
-        segment = AudioSegment.from_file(filename)
-        combined_audio += segment
-
-    # 保存字幕文件为 SRT 格式
-    subs.save(output_path)
+    # demo数据
+    segment_times = []
+    generate_srt_from_text(segment_times, output_path)
--- scripts/main.py
+++ scripts/main.py
@@ -1,37 +1,14 @@
 # novel_test主入口
 from pathlib import *
-from scripts.config_loader import loader_config
-
-
-# 从配置读取小说文本
-def read_novel_from_file():
-    pass
-
-
-# 切分小说为分句
-def novel_split():
-    pass
-
-
-def generate_mp3_from_text():
-    pass
-
-
-def generate_srt_from_text():
-    pass
-
-
-# 对所有分句创建mp3、srt
-def generate_mp3_srt():
-    srt = []
-    mp3 = []
-    # 向空序列附加
-    for part in parts:
-        generate_mp3_from_text()
-        generate_srt_from_text()
-    pass
-
+from config_loader import loader_config
+from generate_mp3_from_text import generate_by_coqui_TTS
+from generate_srt_from_text import generate_srt_from_text
 
 if __name__ == "__main__":
-    parts = []
-    generate_mp3_srt(parts)
+    config = loader_config()
+    mp3_path = config.get("output", {}).get("mp3_path", "")
+    srt_path = config.get("output", {}).get("srt_path", "")
+    speaker_path = config.get("output", {}).get("speaker_path", "")
+
+    segment_times = generate_by_coqui_TTS(mp3_path, speaker_path)
+    generate_srt_from_text(segment_times, srt_path)
